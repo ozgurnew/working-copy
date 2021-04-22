30afc23cf53a0994354fce64b03eba3b34df0f62
ozgurnew <61843355+ozgurnew@users.noreply.github.com>
2021-04-22 09:10:48 +0300

    Updated Variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,35 @@
+#Azure Region
 variable "location" {
   type    = string
   default = "westus2"
 }
+
+#ENV datadogApiKey https://app.datadoghq.com/account/settings#api
 variable "datadog_api_key" {
   type = string
 }
 
+#ENV datadogAppKey https://app.datadoghq.com/access/application-keys
 variable "datadog_app_key" {
   type = string
 }
 
+#ENV armTenantId 
 variable "tenant_name" {
   type = string
 }
 
+#ENV armClientId 
 variable "client_id" {
   type = string
 }
+
+#ENV armClientSecret
 variable "client_secret" {
   type = string
 }
 
+#ENV uniqueStorage
 variable "unique_storage" {
    type = string
   default = "tfpollinatestorage"
